5b68fb19e1c30375359ffb4b66e7a1e4a4622215
efikalti <efikalti@gmail.com>
2015-06-23 12:56:46 +0300

    LAM-19 changed tabs to spaces

--- example/data-generator.py
+++ example/data-generator.py
@@ -3,38 +3,38 @@ import random
 
 """ Create a random message with 20 words and 5 tags.
 
-	Create 2 lists, one for the message and one for the tags.
-	Select 25 random numbers in range 1 - #number of file lines.
-	Read the file and for each line selected, add it to the appropriate list.
-	Finally print the message with the tags.
+    Create 2 lists, one for the message and one for the tags.
+    Select 25 random numbers in range 1 - #number of file lines.
+    Read the file and for each line selected, add it to the appropriate list.
+    Finally print the message with the tags.
 """
 
 
 def main():
-	filename = '/usr/share/dict/american-english'
-	lines = list()
-	hashtags = list()
-	message = ''
-	tags = ''
-	f = open(filename)
+    filename = '/usr/share/dict/american-english'
+    lines = list()
+    hashtags = list()
+    message = ''
+    tags = ''
+    f = open(filename)
 
-	for i in range(20):
-		j = random.randrange(1, 99171)
-		lines.append(j)
+    for i in range(20):
+        j = random.randrange(1, 99171)
+        lines.append(j)
 
-	for i in range(5):
-		j = random.randrange(1, 99171)
-		hashtags.append(j)
+    for i in range(5):
+        j = random.randrange(1, 99171)
+        hashtags.append(j)
 
-	i = 0
-	for line in f:
-		if i in lines:
-			message = message + line.strip() + ' '
-		if i in hashtags:
-			tags = tags + '#' + line.strip() + ' '
-		i += 1
-	f.close()
-	print message, tags
+    i = 0
+    for line in f:
+        if i in lines:
+            message = message + line.strip() + ' '
+        if i in hashtags:
+            tags = tags + '#' + line.strip() + ' '
+        i += 1
+    f.close()
+    print message, tags
 
 if __name__ == '__main__':
-	main()
+    main()
