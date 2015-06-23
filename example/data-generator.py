22705b47a874213b5619f62a19d830779686fa83
efikalti <efikalti@gmail.com>
2015-06-23 12:25:14 +0300

    [LAM-18] fixed quality errors and added documentation

--- example/data-generator.py
+++ example/data-generator.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python
 import random
 
+""" Create a random message with 20 words and 5 tags.
+
+	Create 2 lists, one for the message and one for the tags.
+	Select 25 random numbers in range 1 - #number of file lines.
+	Read the file and for each line selected, add it to the appropriate list.
+	Finally print the message with the tags.
+"""
+
+
 def main():
 	filename = '/usr/share/dict/american-english'
 	lines = list()
 	hashtags = list()
 	message = ''
 	tags = ''
-	f=open(filename)
+	f = open(filename)
 
 	for i in range(20):
-		j = random.randrange(1,99171)
+		j = random.randrange(1, 99171)
 		lines.append(j)
 
 	for i in range(5):
-		j = random.randrange(1,99171)
+		j = random.randrange(1, 99171)
 		hashtags.append(j)
 
 	i = 0
@@ -25,7 +34,7 @@ def main():
 			tags = tags + '#' + line.strip() + ' '
 		i += 1
 	f.close()
-	print message,tags
+	print message, tags
 
 if __name__ == '__main__':
 	main()
