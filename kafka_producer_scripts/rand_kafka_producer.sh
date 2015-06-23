05434c4dabf7ec2dfe4882288df9c2c277d73464
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-06-23 14:12:37 +0300

    LAM-25 Changed input topic

--- kafka_producer_scripts/rand_kafka_producer.sh
+++ kafka_producer_scripts/rand_kafka_producer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ -f "/root/runrand" ]; do
-   python /root/data-generator.py | /usr/lib/kafka_2.10-0.8.2.1/bin/kafka-console-producer.sh --broker-list snf-661306:9092 --topic input-tweets
+   python /root/data-generator.py | /usr/lib/kafka_2.10-0.8.2.1/bin/kafka-console-producer.sh --broker-list 192.168.0.3:9092 --topic input
    sleep 1
 done
 
