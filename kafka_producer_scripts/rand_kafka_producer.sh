52fc05850ba1f6bc6e80b04a65a396049d4510ee
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-06-25 17:44:10 +0300

    LAM-25 Changed kafka install dir

--- kafka_producer_scripts/rand_kafka_producer.sh
+++ kafka_producer_scripts/rand_kafka_producer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ -f "/root/runrand" ]; do
-   python /root/data-generator.py | /usr/lib/kafka_2.10-0.8.2.1/bin/kafka-console-producer.sh --broker-list 192.168.0.3:9092 --topic input
+   python /root/data-generator.py | /usr/local/kafka/bin/kafka-console-producer.sh --broker-list 192.168.0.3:9092 --topic input
    sleep 1
 done
 
