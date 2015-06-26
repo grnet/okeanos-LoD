1fc3472a0669b08982f78ee0f7fcdf64bbea7373
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-06-26 11:18:58 +0300

    LAM-22 Added comments to the kafka stream input consumer scripts.

--- example/create-input-topic.sh
+++ example/create-input-topic.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# This script should be executed on the machine on which zookeeper is running. To execute it on another machine, change the
+# "localhost" on the following command with the IP address of the machine that runs zookeeper.
+
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic input;
 
--- example/kafka-stream-input-consumer.sh
+++ example/kafka-stream-input-consumer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# Change zookeeper-host and application-master-host with the ips of the zookeeper host and the Apache Flink Application Master host. Tis command will create a live feed from topic input
-# to the port 9999 of the Application Master host. The stream job should then read the port 9999. The Application Master is responsible for distributing the data to the appropriate machines.
+# Change "zookeeper-host" with the IP address of the machine that runs Apache Zookeeper.
+# Change "application-master-host" with the IP address of the machine tha runs the Apache Flink Application Master.
+# This command will create a live feed of data, from the topic "input" to the port 9999 of the Application Master host.
+# The stream job will then read the data from the port 9999.
+# The Application Master is responsible for distributing the data to the machines that run the Apache Flink job.
+
 /usr/local/kafka/bin/kafka-console-consumer.sh --consumer.config /usr/local/kafka/config/consumer.properties --zookeeper zookeeper-host:2181  --topic input | nc -lk application-master-host -p 9999;
