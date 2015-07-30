1427b373241db8bdc264aeb145e63f588d232108
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-07-30 18:10:58 +0300

    LAM-53 Added templated settings.py. Merged django-install.yml to main.yml task. Refactored handlers that migrate the databases.

--- webapp/backend/models.py
+++ webapp/backend/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.CharField("UUID", primary_key=True, null=False, blank=False, unique=True, default="", max_length=255,
-                          help_text="Unique user id asign by Astakos")
-    token = models.CharField("Okeanos Token", null=True, blank=True, unique=True, default="", max_length=64,
-                             help_text="Token provided by ~okeanos.")
+    id = models.CharField("UUID", primary_key=True, null=False, blank=False, unique=True, default="", max_length=255, help_text="Unique user id asign by Astakos")
+    token = models.CharField("Okeanos Token", null=True, blank=True, unique=True, default="", max_length=64, help_text="Token provided by ~okeanos.")
 
 class Project(models.Model):
     id = models.AutoField("Project ID", primary_key=True, null=False, blank=False, unique=True, default="", help_text="Project id provided by kamaki.")
-    description = models.TextField("Project Description", null=True, blank=True, unique=False, default="",
-                                   help_text="The description of a project.")
+    description = models.TextField("Project Description", null=True, blank=True, unique=False, default="", help_text="The description of a project.")
 
 class Cluster(models.Model):
     id = models.AutoField("Cluster ID", primary_key=True, null=False, help_text="Auto-increment cluster id.")
-    # OneToOneField is a ForeignKey with unique=True. Django recommends using OneToOne Field instead of a ForeignKey
+    # OneToOneField is a ForeignKey with unique=True. Django recommends using OneToOneField instead of a ForeignKey
     # with unique=True.
     master_server = models.OneToOneField('Server', null=True, blank=True, on_delete=models.CASCADE)
 
