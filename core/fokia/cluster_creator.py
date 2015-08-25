a8af61ad7c2980f236dd82e9c3ba09aa4549f23b
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-08-25 12:29:39 +0300

    Refactor cluster_creator

--- core/fokia/cluster_creator.py
+++ core/fokia/cluster_creator.py
@@ -4,89 +4,108 @@ import os
 import inspect
 from fokia.provisioner import Provisioner
 from fokia.ansible_manager import Manager
+# script_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+script_path = '/var/www/okeanos-LoD/core/fokia'
+
 
 def get_cluster_details(cluster_id):
     """
     :param cluster_id: id of the cluster
     :returns: the details of the cluster after retrieving them from the database.
     """
-    #TODO
-    #1. create a query for the table cluster requesting the cluster info with this id
-    #2. parse the answer, create a dictionary object with this format:
+    # TODO
+    # 1. create a query for the table cluster requesting the cluster info with this id
+    # 2. parse the answer, create a dictionary object with this format:
     """
     {'nodes':[master_id,node1_id,node2_id,...], 'vpn':vpn_id}
     """
-    #3. return dictionary, return null if the query did not return any answer.
+    # 3. return dictionary, return null if the query did not return any answer.
     return None
 
 
-if __name__ == "__main__":
+def create_cluster(cloud_name='lambda', master_name='lambda-master',
+                   slaves=1, vcpus_master=4, vcpus_slave=4,
+                   ram_master=4096, ram_slave=4096, disk_master=40, disk_slave=40,
+                   ip_alloc=1, network_request=1, project_name='lambda.grnet.gr'):
     start_time = time.time()
-    script_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-    parser = argparse.ArgumentParser(description="Okeanos VM provisioning")
-    parser.add_argument('--cloud', type=str, dest="cloud", default="~okeanos")
-    parser.add_argument('--project-name', type=str, dest="project_name",
-                        default="lambda.grnet.gr")
+    provisioner = Provisioner(cloud_name=cloud_name)
+    provisioner.create_lambda_cluster(vm_name=master_name,
+                                      slaves=slaves,
+                                      vcpus_master=vcpus_master,
+                                      vcpus_slave=vcpus_slave,
+                                      ram_master=ram_master,
+                                      ram_slave=ram_slave,
+                                      disk_master=disk_master,
+                                      disk_slave=disk_slave,
+                                      ip_allocation=ip_alloc,
+                                      network_request=network_request,
+                                      project_name=project_name)
 
-    parser.add_argument('--slaves', type=int, dest='slaves', default=2)
-    parser.add_argument('--vcpus_master', type=int, dest='vcpus_master', default=4)
-    parser.add_argument('--vcpus_slave', type=int, dest='vcpus_slave', default=4)
-    parser.add_argument('--ram_master', type=int, dest='ram_master', default=4096)  # in MB
-    parser.add_argument('--ram_slave', type=int, dest='ram_slave', default=4096)  # in MB
-    parser.add_argument('--disk_master', type=int, dest='disk_master', default=40)  # in GB
-    parser.add_argument('--disk_slave', type=int, dest='disk_slave', default=40)  # in GB
-    parser.add_argument('--ip_alloc', type=str, dest='ip_alloc', default="master",
-                        help="Choose between none, master, all")
-    parser.add_argument('--network_request', type=int, dest='network_request', default=1)
-    parser.add_argument('--image_name', type=str, dest='image_name', default='debian')
-    parser.add_argument('--action', type=str, dest='action', default='create')
-    parser.add_argument('--cluster_id', type=int, dest='cluster_id', default=0)
+    provisioner_response = provisioner.get_cluster_details()
+    master_id = provisioner_response['nodes']['master']['id']
+    master_ip = provisioner.get_server_private_ip(master_id)
+    provisioner_response['nodes']['master']['internal_ip'] = master_ip
+    # slave_ids = [slave['id'] for slave in provisioner_response['nodes']['slaves']]
+    for i, slave in enumerate(provisioner_response['nodes']['slaves']):
+        slave_ip = provisioner.get_server_private_ip(slave['id'])
+        provisioner_response['nodes']['slaves'][i]['internal_ip'] = slave_ip
+    provisioner_response['pk'] = provisioner.get_private_key()
 
-    args = parser.parse_args()
-    provisioner = Provisioner(cloud_name=args.cloud)
+    print 'response =', provisioner_response
+    provisioner_time = time.time()
 
-    if args.action == 'create':
-        provisioner.create_lambda_cluster('lambda-master', slaves=args.slaves,
-                                          vcpus_master=args.vcpus_master,
-                                          vcpus_slave=args.vcpus_slave,
-                                          ram_master=args.ram_master,
-                                          ram_slave=args.ram_slave,
-                                          disk_master=args.disk_master,
-                                          disk_slave=args.disk_slave,
-                                          ip_allocation=args.ip_alloc,
-                                          network_request=args.network_request,
-                                          project_name=args.project_name)
+    manager = Manager(provisioner_response)
+    manager.create_inventory()
+    # manager.run_playbook(
+    #     playbook_file=script_path +
+    #                   "/../../ansible/playbooks/test/testinventory.yml", tags=['hosts'])
+    # manager.run_playbook(
+    #     playbook_file=script_path +
+    #                   "/../../ansible/playbooks/test/testproxy.yml", tags=['install'])
 
-        provisioner_response = provisioner.get_cluster_details()
-        master_id = provisioner_response['nodes']['master']['id']
-        master_ip = provisioner.get_server_private_ip(master_id)
-        provisioner_response['nodes']['master']['internal_ip'] = master_ip
-        slave_ids = [slave['id'] for slave in provisioner_response['nodes']['slaves']]
-        for i, slave in enumerate(provisioner_response['nodes']['slaves']):
-            slave_ip = provisioner.get_server_private_ip(slave['id'])
-            provisioner_response['nodes']['slaves'][i]['internal_ip'] = slave_ip
-        provisioner_response['pk'] = provisioner.get_private_key()
+    ansible_result = manager.run_playbook(
+        playbook_file=script_path + "/../../ansible/playbooks/cluster-install.yml")
 
-        print 'response =', provisioner_response
-        provisioner_time = time.time()
+    manager.cleanup()
 
-        manager = Manager(provisioner_response)
-        manager.create_inventory()
-        # manager.run_playbook(playbook_file=script_path + "/../../ansible/playbooks/test/testinventory.yml", tags=['hosts'])
-        # manager.run_playbook(playbook_file=script_path + "/../../ansible/playbooks/test/testproxy.yml", tags=['install'])
+    provisioner_duration = provisioner_time - start_time
+    ansible_duration = time.time() - provisioner_time
 
-        manager.run_playbook(playbook_file=script_path + "/../../ansible/playbooks/cluster-install.yml")
+    print 'VM provisioning took', round(provisioner_duration), 'seconds'
+    print 'Ansible playbooks took', round(ansible_duration), 'seconds'
+    print 'Ansible result', ansible_result
 
-        manager.cleanup()
+    return ansible_result
 
-        provisioner_duration = provisioner_time - start_time
-        ansible_duration = time.time() - provisioner_time
 
-        print 'VM provisioning took', round(provisioner_duration), 'seconds'
-        print 'Ansible playbooks took', round(ansible_duration), 'seconds'
+def destroy_cluster(cloud_name, cluster_id):
+    provisioner = Provisioner(cloud_name=cloud_name)
+    details = get_cluster_details(cluster_id=cluster_id)
+    if details is not None:
+        provisioner.delete_lambda_cluster(details)
 
-    elif args.action == 'delete':
-        details = get_cluster_details(args['cluster_id'])
-        if details != None:
-            provisioner.delete_lambda_cluster(details)
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description="Okeanos VM provisioning")
+    # parser.add_argument('--cloud', type=str, dest="cloud", default="lambda")
+    # parser.add_argument('--project-name', type=str, dest="project_name",
+    #                     default="lambda.grnet.gr")
+    #
+    # parser.add_argument('--slaves', type=int, dest='slaves', default=2)
+    # parser.add_argument('--vcpus_master', type=int, dest='vcpus_master', default=4)
+    # parser.add_argument('--vcpus_slave', type=int, dest='vcpus_slave', default=4)
+    # parser.add_argument('--ram_master', type=int, dest='ram_master', default=4096)  # in MB
+    # parser.add_argument('--ram_slave', type=int, dest='ram_slave', default=4096)  # in MB
+    # parser.add_argument('--disk_master', type=int, dest='disk_master', default=40)  # in GB
+    # parser.add_argument('--disk_slave', type=int, dest='disk_slave', default=40)  # in GB
+    # parser.add_argument('--ip_alloc', type=str, dest='ip_alloc', default="master",
+    #                     help="Choose between none, master, all")
+    # parser.add_argument('--network_request', type=int, dest='network_request', default=1)
+    # parser.add_argument('--image_name', type=str, dest='image_name', default='debian')
+    # parser.add_argument('--action', type=str, dest='action', default='create')
+    # parser.add_argument('--cluster_id', type=int, dest='cluster_id', default=0)
+    #
+    # args = parser.parse_args()
+
+    create_cluster()
