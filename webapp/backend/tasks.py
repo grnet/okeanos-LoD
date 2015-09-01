2f05e8a52e499625c5717b4189bcd1859e0c4ec9
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-09-01 13:09:32 +0300

    Moved kamaki interactions from tasks to Fokia.

--- webapp/backend/tasks.py
+++ webapp/backend/tasks.py
@@ -1,8 +1,8 @@
 from celery import shared_task
 
 from kamaki.clients import ClientError
-from kamaki.clients.astakos import AstakosClient
-from kamaki.clients.cyclades import CycladesComputeClient, CycladesNetworkClient
+
+import fokia.utils
 
 from .events import set_lambda_instance_status
 from .models import LambdaInstance
@@ -20,27 +20,9 @@ def lambda_instance_start(instance_uuid, auth_url, auth_token, master_id, slave_
     :param slave_ids: The ~okeanos ids of the VMs that act as the slave nodes.
     """
 
-    # Create cyclades client.
-    cyclades_compute_url = AstakosClient(auth_url, auth_token).get_endpoint_url(
-        CycladesComputeClient.service_type)
-    cyclades = CycladesComputeClient(cyclades_compute_url, auth_token)
-
     try:
-        # Start all slave nodes.
-        for slave_id in slave_ids:
-            if cyclades.get_server_details(slave_id)["status"] != "ACTIVE":
-                cyclades.start_server(slave_id)
-
-        # Wait until all slave nodes have been started.
-        for slave_id in slave_ids:
-            cyclades.wait_server(slave_id, current_status="STOPPED")
-
-        # Start master node.
-        if cyclades.get_server_details(master_id)["status"] != "ACTIVE":
-            cyclades.start_server(master_id)
-
-        # Wait until master node has been started.
-        cyclades.wait_server(master_id, current_status="STOPPED")
+        # Start the VMs of the specified lambda instance.
+        fokia.lambda_instance_start(auth_url, auth_token, master_id, slave_ids)
 
         # Update lambda instance status on the database to started.
         set_lambda_instance_status.delay(instance_uuid, LambdaInstance.STARTED)
@@ -61,27 +43,9 @@ def lambda_instance_stop(instance_uuid, auth_url, auth_token, master_id, slave_i
     :param slave_ids: The ~okeanos ids of the VMs that act as the slave nodes.
     """
 
-    # Create cyclades client.
-    cyclades_compute_url = AstakosClient(auth_url, auth_token).get_endpoint_url(
-        CycladesComputeClient.service_type)
-    cyclades = CycladesComputeClient(cyclades_compute_url, auth_token)
-
     try:
-        # Stop master node.
-        if cyclades.get_server_details(master_id)["status"] != "STOPPED":
-            cyclades.shutdown_server(master_id)
-
-        # Wait until master node has been stopped.
-        cyclades.wait_server(master_id, current_status="ACTIVE")
-
-        # Stop all slave nodes.
-        for slave_id in slave_ids:
-            if cyclades.get_server_details(slave_id)["status"] != "STOPPED":
-                cyclades.shutdown_server(slave_id)
-
-        # Wait until all slave nodes have been stopeed.
-        for slave_id in slave_ids:
-            cyclades.wait_server(slave_id, current_status="ACTIVE")
+        # Stop the VMs of the specified lambda instance.
+        fokia.lambda_instance_stop(auth_url, auth_token, master_id, slave_ids)
 
         # Update lambda instance status on the database to started.
         set_lambda_instance_status.delay(instance_uuid, LambdaInstance.STOPPED)
@@ -106,44 +70,10 @@ def lambda_instance_destroy(instance_uuid, auth_url, auth_token, master_id, slav
     :param private_network_id: The ~okeanos id of the private network used by the lambda instance.
     """
 
-    # Create cyclades compute client.
-    cyclades_compute_url = AstakosClient(auth_url, auth_token).get_endpoint_url(
-        CycladesComputeClient.service_type)
-    cyclades_compute_client = CycladesComputeClient(cyclades_compute_url, auth_token)
-
-    # Create cyclades network client.
-    cyclades_network_url = AstakosClient(auth_url, auth_token).get_endpoint_url(
-        CycladesNetworkClient.service_type)
-    cyclades_network_client = CycladesNetworkClient(cyclades_network_url, auth_token)
-
     try:
-        # Get the current status of the VMs.
-        master_status = cyclades_compute_client.get_server_details(master_id)["status"]
-        slaves_status = []
-        for slave_id in slave_ids:
-            slaves_status.append(cyclades_compute_client.get_server_details(slave_id)["status"])
-
-        # Destroy all the VMs without caring for properly stopping the lambda services.
-        # Destroy master node.
-        if cyclades_compute_client.get_server_details(master_id)["status"] != "DELETED":
-            cyclades_compute_client.delete_server(master_id)
-
-        # Destroy all slave nodes.
-        for slave_id in slave_ids:
-            if cyclades_compute_client.get_server_details(slave_id)["status"] != "DELETED":
-                cyclades_compute_client.delete_server(slave_id)
-
-        # Wait for all the VMs to be destroyed before destroyed the public ip and the
-        # private network.
-        cyclades_compute_client.wait_server(master_id, current_status=master_status)
-        for i, slave_id in enumerate(slave_ids):
-            cyclades_compute_client.wait_server(slave_id, current_status=slaves_status[i])
-
-        # Destroy the public ip.
-        cyclades_network_client.delete_floatingip(public_ip_id)
-
-        # Destroy the private network.
-        cyclades_network_client.delete_network(private_network_id)
+        # Destroy all VMs, the public ip and the private network of the lambda instance.
+        fokia.lambda_instance_destroy(auth_url, auth_token, master_id, slave_ids, public_ip_id,
+                                      private_network_id)
 
         # Update lambda instance status on the database to destroyed.
         set_lambda_instance_status.delay(instance_uuid, LambdaInstance.DESTROYED)
