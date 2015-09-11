45271aa5b70a3f1922dbcf0c9ee31fc3c02e08bf
Avraam Tsantekidis <avraamt@lab.grid.auth.gr>
2015-09-11 18:17:07 +0300

    New serializer to validate cluster creation request

--- webapp/backend/serializers.py
+++ webapp/backend/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from .models import Application, LambdaInstance, Server, PrivateNetwork
 
+from .models import ProjectFile, LambdaInstance, Server, PrivateNetwork
+from .models import Application, LambdaInstance, Server, PrivateNetwork
 
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -40,3 +41,40 @@ class LambdaInstanceSerializer(serializers.ModelSerializer):
         model = LambdaInstance
         fields = ('id', 'uuid', 'name', 'instance_info', 'status', 'failure_message', 'servers',
                   'private_network')
+
+
+class LambdaInstanceInfo(serializers.Serializer):
+    instance_name = serializers.CharField()
+    master_name = serializers.CharField()
+    project_name = serializers.CharField()
+    ip_allocation = serializers.CharField()
+    slaves = serializers.IntegerField()
+    vcpus_master = serializers.IntegerField()
+    vcpus_slave = serializers.IntegerField()
+    ram_master = serializers.IntegerField()
+    ram_slave = serializers.IntegerField()
+    disk_master = serializers.IntegerField()
+    disk_slave = serializers.IntegerField()
+    network_request = serializers.IntegerField()
+
+    allowed = {
+        "vcpus": {8, 1, 2, 4},
+        "disks": {100, 5, 40, 10, 80, 20, 60},
+        "ram":   {1024, 2048, 4096, 6144, 8192, 512},
+        "disk":  {u'drbd', u'ext_vlmc'}
+    }
+
+    def validate(self, data):
+        if data['vcpus_master'] not in self.allowd['vcpus']:
+            raise serializers.ValidationError("Wrong Number of master vcpus")
+        if data['vcpus_slave'] not in self.allowd['vcpus']:
+            raise serializers.ValidationError("Wrong Number of slave vcpus")
+        if data['ram_master'] not in self.allowd['ram']:
+            raise serializers.ValidationError("Wrong amount of master ram")
+        if data['ram_slave'] not in self.allowd['ram']:
+            raise serializers.ValidationError("Wrong amount of slave ram")
+        if data['disk_master'] not in self.allowd['disk']:
+            raise serializers.ValidationError("Wrong size of master disk")
+        if data['disk_slave'] not in self.allowd['disk']:
+            raise serializers.ValidationError("Wrong size of slave disk")
+
