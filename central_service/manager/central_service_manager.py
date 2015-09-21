f898e7b61fedd331d755defa13d6c9e8568e33b1
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-09-21 15:08:43 +0300

    LAM-132 Minor fixes in central_service_manager.py for sync
    LAM-132 Fixes for travis.

--- central_service/manager/central_service_manager.py
+++ central_service/manager/central_service_manager.py
@@ -2,6 +2,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class CentralServiceManager:
     """
     Class deploying dynamically the central service VM.
@@ -9,28 +10,28 @@ class CentralServiceManager:
     the ~okeanos infrastructure.
     """
 
-    def central_service_create():
+    def central_service_create(self):
         """
         Creates the central service vm and installs the relevant s/w.
         :return:
         """
         raise NotImplementedError
 
-    def central_service_destroy():
+    def central_service_destroy(self):
         """
         Deletes the central service vm.
         :return:
         """
         raise NotImplementedError
 
-    def central_service_start():
+    def central_service_start(self):
         """
         Starts the central service vm if it's not running.
         :return:
         """
         raise NotImplementedError
 
-    def central_service_stop():
+    def central_service_stop(self):
         """
         Stops the central service vm if it's running.
         :return:
