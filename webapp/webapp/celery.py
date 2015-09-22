f7a622ed5c55944071a2fbeb4a56da24e013f1fa
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-09-22 12:28:08 +0300

    Configure RabbitMQ user for Celery

--- webapp/webapp/celery.py
+++ webapp/webapp/celery.py
@@ -9,7 +9,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
 from django.conf import settings
 
-app = Celery('webapp', broker='amqp://', backend='amqp://')
+rabbitmq = 'amqp://lambda:change_me@localhost:5672'
+
+app = Celery('webapp', broker=rabbitmq, backend=rabbitmq)
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
