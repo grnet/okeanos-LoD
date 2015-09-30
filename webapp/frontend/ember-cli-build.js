3699111f43c61c673d20a3b36faa19fcef37f2fe
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-09-30 16:07:13 +0300

    Install bootstrap and handlebars through bower, include them to ember app

--- webapp/frontend/ember-cli-build.js
+++ webapp/frontend/ember-cli-build.js
@@ -19,5 +19,9 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import(app.bowerDirectory + '/handlebars/handlebars.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+
   return app.toTree();
 };
