b2df6d3a6485ce03ef5e8aa88355addcafa2810a
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-09-30 20:53:56 +0300

    LAM-140 Postgres support

--- central_service/app/app/settings.py
+++ central_service/app/app/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lambda_db',
+        'USER': 'lambda',
+        'PASSWORD': 'change_me',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
