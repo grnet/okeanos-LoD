30b1a5868cbbacc6818acea2ebbf25ea482ca3f3
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-10-01 11:50:27 +0300

    LAM-149 Proposed fix.

--- central_service/app/app/celery.py
+++ central_service/app/app/celery.py
@@ -10,7 +10,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 from django.conf import settings
 
-app = Celery('app', broker='amqp://', backend='amqp://')
+rabbitmq = 'amqp://lambda:change_me@localhost:5672'
+app = Celery('app', broker=rabbitmq, backend=rabbitmq)
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
