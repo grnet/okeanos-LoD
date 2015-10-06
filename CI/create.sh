e9d46999740d2523b02408531b2c02346c3be86b
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-10-06 18:00:11 +0300

    LAM-153 Removed ssh keys creation and deletion from continuous integration scripts. Public and private keys paths are provided as input.

--- CI/create.sh
+++ CI/create.sh
@@ -3,11 +3,14 @@
 # ~okeanos token is given as first argument.
 okeanos_token=$1
 
+# Public key path is given as second argument.
+public_key_path=$2
+
+# Private key path is given as third argument.
+private_key_path=$3
+
 # Clone devel branch of ~okeanos-LoD project.
 git clone -b devel https://github.com/grnet/okeanos-LoD.git
-
-# Create a public-private key pair.
-ssh-keygen -t rsa -b 4096 -N "" -f ./.ssh/id_rsa
 
 # Create a python virtual environment and source it.
 virtualenv okeanos_lod_python_environment
@@ -21,15 +24,15 @@ cd ../../
 
 # Create a Service VM.
 cd okeanos-LoD/webapp/manager
-python service_vm_manager.py --action=create --auth_token=$okeanos_token --public_key_path=./.ssh/id_rsa.pub --private_key_path./.ssh/id_rsa
+python service_vm_manager.py --action create --auth_token $okeanos_token --public_key_path "$public_key_path" --private_key_path "$private_key_path"
 cd ../../../
 
 # Create a Lambda Instance.
-echo "$(python manage_lambda_instance.py --action=create --service_vm_name="Service VM" --auth_token=$okeanos_token)"
+echo "$(python manage_lambda_instance.py --action create --service_vm_name "Service VM" --auth_token $okeanos_token)"
 
 # Create a Central VM.
 cd okeanos-LoD/central_service/manager
-python central_service_manager.py --action=create --auth_token=$okeanos_token --public_key_path=./.ssh/id_rsa.pub --private_key_path./.ssh/id_rsa
+python central_service_manager.py --action create --auth_token $okeanos_token --public_key_path "$public_key_path" --private_key_path "$private_key_path"
 cd ../../../
 
 # Applications #
--- CI/destroy.sh
+++ CI/destroy.sh
@@ -3,34 +3,36 @@
 # ~okeanos token is given as first argument.
 okeanos_token=$1
 
+# Public key path is given as second argument.
+public_key_path=$2
+
+# Private key path is given as third argument.
+private_key_path=$3
+
 # Source the python virtual environment.
 source okeanos_lod_python_environment/bin/activate
 
 # Get the id of the Central VM.
-central_vm_id=$(python utils.py --get=id --vm_name="central_service" --auth_token=$okeanos_token)
+central_vm_id=$(python utils.py --get id --vm_name "central_service" --auth_token $okeanos_token)
 
 # Destroy the Central VM.
 cd okeanos-LoD/central_service/manager
-python central_service_manager.py --action=destroy --auth_token=$okeanos_token --vm_id=$central_vm_id --public_key_path=./.ssh/id_rsa.pub --private_key_path./.ssh/id_rsa
+python central_service_manager.py --action destroy --auth_token $okeanos_token --vm_id $central_vm_id --public_key_path "$public_key_path" --private_key_path "$private_key_path"
 cd ../../../
 
 # Destroy the lambda instance.
-echo "$(python manage_lambda_instance.py --action=destroy --service_vm_name="Service VM" --auth_token=$okeanos_token)"
+echo "$(python manage_lambda_instance.py --action destroy --service_vm_name "Service VM" --auth_token $okeanos_token)"
 
 # Get the id of the Service VM.
-service_vm_id=$(python utils.py --get=id --vm_name="Service VM" --auth_token=$okeanos_token)
+service_vm_id=$(python utils.py --get id --vm_name "Service VM" --auth_token $okeanos_token)
 
 # Destroy the Service VM.
 cd okeanos-LoD/webapp/manager
-python service_vm_manager.py --action=destroy --auth_token=$okeanos_token --vm_id=$service_vm_id --public_key_path=./.ssh/id_rsa.pub --private_key_path./.ssh/id_rsa
+python service_vm_manager.py --action destroy --auth_token $okeanos_token --vm_id $service_vm_id --public_key_path "$public_key_path" --private_key_path "$private_key_path"
 cd ../../../
 
 # Destroy the python virtual environment.
 rm -r okeanos_lod_python_environment
-
-# Delete the public and private keys.
-rm ./.ssh/id_rsa
-rm ./.ssh/id_rsa.pub
 
 # Delete the cloned repository.
 $(yes | rm -r okeanos-LoD/)
