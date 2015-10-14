c4aca9ab9feb6e4be61b72950c50efaf714d84b2
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-10-14 17:03:01 +0300

    Refactor lambda instance model

--- webapp/frontend/app/models/lambda-instance.js
+++ webapp/frontend/app/models/lambda-instance.js
@@ -1,17 +1,18 @@
 import DS from "ember-data";
 
-// Information about user
+// Information about lambda instance
 var LambdaInstance = DS.Model.extend({
-  name: DS.attr(),
-  project_name : DS.attr('string'),  // project of the lambda instance
-  instance_name : DS.attr('string'),    // name of the lambda instance
-  slaves : DS.attr('number'),     // size of cluster
-  vcpus_master : DS.attr('number'),     // cpus of master
-  vcpus_slave : DS.attr('number'),		  // cpus of each slave00
-  ram_master : DS.attr('number'),		    // disk for master
-  ram_slave : DS.attr('number'),		    // ram of each slave
-  disk_master : DS.attr('number'),		  // disk of master
-  disk_slave : DS.attr('number'),		    // disk of each slave
+  name: DS.attr(),                  // name of the lambda instance
+  project_name: DS.attr(),          // project of the lambda instance
+  slaves: DS.attr('number'),        // number of slaves
+  master_name: DS.attr(),           // name of master
+  vcpus_master: DS.attr('number'),  // cpus of master
+  vcpus_slave: DS.attr('number'),		// cpus of each slave
+  ram_master: DS.attr('number'),		// disk for master
+  ram_slave: DS.attr('number'),		  // ram of each slave
+  disk_master: DS.attr('number'),		// disk of master
+  disk_slave: DS.attr('number'),		// disk of each slave
+  public_key_name: DS.attr()        // ~okeanos public keys
 });
 
 export default LambdaInstance;
