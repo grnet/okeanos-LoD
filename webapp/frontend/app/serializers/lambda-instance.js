4f079f7cadadf33c50bae0749ee54cfd9114e94f
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-10-15 18:39:48 +0300

    Serialize lambda instance status fields

--- webapp/frontend/app/serializers/lambda-instance.js
+++ webapp/frontend/app/serializers/lambda-instance.js
@@ -1,14 +1,17 @@
 import serializer from 'frontend/serializers/application';
 
-export default serializer.extend({
-
+export default serializer.extend(DS.EmbeddedRecordsMixin, {
   normalizeSingleResponse: function (store, primaryModelClass, payload, id, requestType) {
     payload.data[0].id = payload.data[0].attributes.info.id;
-    payload.data[0].attributes = payload.data[0].attributes.info;
-    for (var k in payload.data[0].attributes.instance_info) {
-      payload.data[0].attributes[k] = payload.data[0].attributes.instance_info[k];
+    payload.data[0].attributes.name = payload.data[0].attributes.info.name;
+    for (var k in payload.data[0].attributes.info.instance_info) {
+      payload.data[0].attributes[k] = payload.data[0].attributes.info.instance_info[k];
+    }
+    for (var k in payload.data[0].attributes.status) {
+      var key = 'status_' + k;
+      console.debug(key);
+      payload.data[0].attributes[key] = payload.data[0].attributes.status[k];
     }
     return this._super(store, primaryModelClass, payload, id, requestType);
   }
-
 });
