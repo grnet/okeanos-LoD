71de473f2d3258f48725076d1e21d12bdf65ffd7
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-10-15 16:36:03 +0300

    LAM-73 Corrections in imports.

--- webapp/frontend/app/adapters/user-public-key.js
+++ webapp/frontend/app/adapters/user-public-key.js
@@ -1,8 +1,7 @@
-import Ember from "ember";
 import GenericAdapter from 'frontend/adapters/application';
 
 export default GenericAdapter.extend({
   shouldReloadAll: function(){
     return true;
-  },
+  }
 });
