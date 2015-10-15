eae273c281b211a2c51006e534ebb3e4fea1e9e2
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-10-16 00:09:00 +0300

    Status and failure message on web ui.

--- webapp/frontend/app/serializers/lambda-instances.js
+++ webapp/frontend/app/serializers/lambda-instances.js
@@ -5,8 +5,14 @@ export default DS.JSONAPISerializer.extend({
   normalizeArrayResponse: function(store, primaryModelClass, payload, id, requestType) {
     var pluralTypeKey = Ember.Inflector.inflector.pluralize(primaryModelClass.modelName);
     for (var i=0;i<payload.data.length;++i) {
-      payload.data[i].attributes = jQuery.extend(true, {}, payload.data[i]);
+      var status = Ember.$.extend(true, {}, payload.data[i].status);
+      console.debug('status');
+      console.debug(status);
+      delete payload.data[i].status;
+      payload.data[i].attributes = Ember.$.extend(true, {}, payload.data[i]);
       payload.data[i].attributes.uuid = payload.data[i].id;
+      payload.data[i].attributes.status = status.detail;
+      payload.data[i].attributes.failure_message = status.failure_message;
       payload.data[i].type = pluralTypeKey;
     }
     delete payload.status;
