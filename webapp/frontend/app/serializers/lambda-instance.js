3f490434171551541c878768a8ec7e440b454fde
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-10-15 10:23:01 +0300

    Fixed style issues

--- webapp/frontend/app/serializers/lambda-instance.js
+++ webapp/frontend/app/serializers/lambda-instance.js
@@ -1,12 +1,14 @@
-import serializer from './application'
+import serializer from 'frontend/serializers/application';
 
 export default serializer.extend({
 
   normalizeSingleResponse: function (store, primaryModelClass, payload, id, requestType) {
     payload.data[0].id = payload.data[0].attributes.info.id;
     payload.data[0].attributes = payload.data[0].attributes.info;
-    for (var k in payload.data[0].attributes.instance_info) payload.data[0].attributes[k] = payload.data[0].attributes.instance_info[k];
+    for (var k in payload.data[0].attributes.instance_info) {
+      payload.data[0].attributes[k] = payload.data[0].attributes.instance_info[k];
+    }
     return this._super(store, primaryModelClass, payload, id, requestType);
   }
-  
+
 });
