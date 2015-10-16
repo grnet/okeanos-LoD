e7f5b2727deb2cadb31fff141401e34d03caa055
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2015-10-16 11:30:06 +0300

    removed clusters file

--- webapp/frontend/app/adapters/clusters.js
+++
@@ -1,7 +0,0 @@
-import ApplicationAdapter from './application';
-import config from '../config/environment';
-
-export default ApplicationAdapter.extend({
-  host: config.host + ':80',
-  namespace: 'api/lambda-instances/'
-});
