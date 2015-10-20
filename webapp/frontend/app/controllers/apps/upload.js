58e9d26fef0a36fd3c3e5939507a2b8a009eef08
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2015-10-20 16:36:26 +0300

    upload progress bar completed

--- webapp/frontend/app/controllers/apps/upload.js
+++ webapp/frontend/app/controllers/apps/upload.js
@@ -28,6 +28,11 @@ var UploadController = Ember.Controller.extend({
 
       var data = new FormData(document.getElementById("upload-app-form"));
 
+      var progress = document.getElementById('progress');
+      var progress_text = document.getElementById('progress_text');
+      progress.style.width = 0;
+
+
       Ember.$.ajax({
         url: postUrl,
         headers: headers,
@@ -35,23 +40,43 @@ var UploadController = Ember.Controller.extend({
         processData: false,
         contentType: false,
         data: data,
+
+        xhr: function()
+        {
+          var xhr = new window.XMLHttpRequest();
+          //Upload progress
+          xhr.upload.addEventListener("progress", function(evt){
+            if (evt.lengthComputable) {
+              var percentComplete = evt.loaded / evt.total;
+              //Do something with upload progress
+              console.log(percentComplete);
+              progress.style.width = percentComplete * 100 + '%';
+              progress_text.innerHTML =  percentComplete * 100 + '%';
+            }
+          }, false);
+          return xhr;
+        },
+
         success: function(){
           console.log("success");
           _this.set("successUpload", true);
           _this.set("sameUpload", false);
           _this.set("serverError", false);
+          //progress.style.width = 0 + '%';
         },
         statusCode: {
           400: function() {
             _this.set("sameUpload", true);
             _this.set("serverError", false);
             _this.set("successUpload", false);
+            //progress.style.width = 0 + '%';
           }
         },
         error: function() {
           _this.set("sameUpload", false);
           _this.set("serverError", true);
           _this.set("successUpload", false);
+          //progress.style.width = 0 + '%';
         }
       });
     },
