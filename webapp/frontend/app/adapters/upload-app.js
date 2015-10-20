6f2c44b739636c8ae732a45f64df1b328ede7af5
efikalti <efikalti@gmail.com>
2015-10-20 11:26:27 +0300

    replaces port var

--- webapp/frontend/app/adapters/upload-app.js
+++ webapp/frontend/app/adapters/upload-app.js
@@ -2,6 +2,6 @@ import DS from "ember-data";
 import ENV from 'frontend/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: ENV.host + ':' + ENV.port,
+  host: ENV.host + ':80',
   namespace: 'api/apps/',
  });
