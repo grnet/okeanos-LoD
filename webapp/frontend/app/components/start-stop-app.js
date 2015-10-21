fec14315e5bb7c7dbe643c421f5a562a0193387d
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2015-10-21 16:12:12 +0300

    added stop functionality too

--- webapp/frontend/app/components/start-stop-app.js
+++ webapp/frontend/app/components/start-stop-app.js
@@ -6,8 +6,16 @@ export default Ember.Component.extend({
       //send request to start application
       app.set('application_id', this.get("application-id"));
       app.set('lambda_instance_id', this.get("instance-id"));
+      app.set('call', "start");
       app.save();
-
+      return false;
+    },
+    stop(app) {
+      //send request to start application
+      app.set('application_id', this.get("application-id"));
+      app.set('lambda_instance_id', this.get("instance-id"));
+      app.set('call', "stop");
+      app.save();
       return false;
     },
   }
--- webapp/frontend/app/models/app.js
+++ webapp/frontend/app/models/app.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   application_id: DS.attr('string'),
   lambda_instance_id: DS.attr('string'),
+  call: DS.attr('string'),
 });
