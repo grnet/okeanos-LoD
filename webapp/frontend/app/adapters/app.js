9e670086d4041e10b78fdc827f148fc5dcf23c60
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2015-10-23 10:23:02 +0300

    removed unused files

--- webapp/frontend/app/adapters/app.js
+++
@@ -1,7 +0,0 @@
-import DS from "ember-data";
-import ENV from 'frontend/config/environment';
-
-export default DS.JSONAPIAdapter.extend({
-  host: ENV.host + ':' + ENV.port,
-  namespace: 'api',
- });
