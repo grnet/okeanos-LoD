d2b9d61a64d4079f5be802bbca7b6fafec95f71e
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-10-23 02:28:14 +0200

    Live refresh of the list.

--- webapp/frontend/app/routes/lambda-instances/index.js
+++ webapp/frontend/app/routes/lambda-instances/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import ENV from 'frontend/config/environment';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('lambda-instance');
+  model: function(params) {
+    this.poll = Ember.run.later(this, function () {
+      this.model(params).reload();
+    }, ENV.refresh_interval);
+
+    return this.store.findAll('lambda-instance', params);
   }
 });
