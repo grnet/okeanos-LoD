aeb340d93d4c0cbb8b9f072d4f103ee4f73a95e8
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-10-24 04:37:31 +0300

    Removed some deprecations

--- webapp/frontend/app/adapters/lambda-app.js
+++ webapp/frontend/app/adapters/lambda-app.js
@@ -3,5 +3,8 @@ import LoDAdapter from 'frontend/adapters/application';
 export default LoDAdapter.extend({
   pathForType: function () {
     return 'apps';
+  },
+  shouldBackgroundReloadRecord: function(){
+    return true;
   }
 });
