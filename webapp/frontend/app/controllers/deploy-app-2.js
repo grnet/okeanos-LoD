8e1fdfab3e9df2f32ee21e46b325a2b4ce2d37e0
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-10-25 18:14:43 +0200

    Deploy apps on a selected instance

---
+++ webapp/frontend/app/controllers/deploy-app-2.js
@@ -0,0 +1,24 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+  actions: {
+    deploy(application_id) {
+      var ids = this.get('ids');
+      ids.push(application_id);
+      var applications;
+      applications = this.store.filter('lambda-app', {},
+        function (application) {
+          //if (application.get('status_code') !== 0) {
+          //  return false;
+          //}
+          let id = application.get('id');
+          for (var i = 0; i < ids.length; i++) {
+            if (id === ids[i]) {
+              return false;
+            }
+          }
+          return true;
+        });
+    }
+  }
+});
---
+++ webapp/frontend/app/routes/deploy-app-2.js
@@ -0,0 +1,43 @@
+import Ember from "ember";
+import LoDRoute from 'frontend/routes/application';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+var ids;
+
+export default LoDRoute.extend(AuthenticatedRouteMixin, {
+
+  beforeModel: function () {
+    let params = this.paramsFor(this.routeName);
+    return this.store.findRecord('lambda-instance', params.instance_uuid);
+  },
+
+  model: function (params) {
+    ids = this.store.peekAll('lambda-app').getEach('id');
+
+    return Ember.RSVP.hash({
+      instance: this.store.findRecord('lambda-instance', params.instance_uuid),
+      applications: this.store.filter('lambda-app', {},
+        function (application) {
+          //if (application.get('status_code') !== 0) {
+          //  return false;
+          //}
+          let id = application.get('id');
+          for (var i = 0; i < ids.length; i++) {
+            if (id === ids[i]) {
+              return false;
+            }
+          }
+          return true;
+        }),
+      app: this.store.createRecord('app-action', {})
+    });
+  },
+
+  setupController: function (controller, model) {
+    controller.set('applications', model.applications);
+    controller.set('instance', model.instance);
+    controller.set('app', model.app);
+    controller.set('ids', ids);
+  }
+
+});
