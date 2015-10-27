64b60a48c54fd495f62ba2c39eb0565be01337a9
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2015-10-27 15:01:27 +0200

    upload instace model to remove app after withdraw

--- webapp/frontend/app/components/app-action.js
+++ webapp/frontend/app/components/app-action.js
@@ -53,6 +53,7 @@ export default Ember.Component.extend({
         app.save();
         this.set("request", true);
         this.set("message", "Your request to withdraw the application was successfully sent to the server.");
+        this.sendAction('withdraw');
       }
       return false;
     }
