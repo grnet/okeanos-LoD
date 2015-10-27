778d0a8bc45e1d64bc87096dcc83d224518fa339
efikalti <efikalti@gmail.com>
2015-10-27 19:26:21 +0200

    display message on top of page, add delay for message and action to dismiss it.

--- webapp/frontend/app/controllers/lambda-app.js
+++ webapp/frontend/app/controllers/lambda-app.js
@@ -7,7 +7,20 @@ export default Ember.Controller.extend({
       var _this = this;
       Ember.run.later((function () {
         _this.store.unloadAll('lambda-instance');
+        _this.set("request", false);
       }), 2000);
+    },
+    start_stop: function()
+    {
+      var _this = this;
+      Ember.run.later((function () {
+        _this.set("request", false);
+      }), 4000);
+    },
+    close_alert: function()
+    {
+      var alert = document.getElementById('alert');
+      alert.hidden=true;
     }
-  }
+  },
 });
