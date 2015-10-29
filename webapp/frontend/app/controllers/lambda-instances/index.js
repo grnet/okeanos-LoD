8e7554913bc9586a74f4da6c12980960bc322301
efikalti <edourden@hotmail.com>
2015-10-29 22:25:34 +0200

    removed unused vars and functions from index of list instances

--- webapp/frontend/app/controllers/lambda-instances/index.js
+++ webapp/frontend/app/controllers/lambda-instances/index.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.ArrayController.extend({
-  verify: true,
-  confirm: false,
   queryParams: ["page", "perPage"],
 
   page: 1,
@@ -24,41 +22,4 @@ export default Ember.ArrayController.extend({
 
   totalPagesBinding: "pagedContent.totalPages",
 
-  actions: {
-    close_alert: function()
-    {
-      var alert = document.getElementById('alert');
-      alert.hidden=true;
-    },
-    start_stop: function()
-    {
-      this.set("confirm", false);
-      var _this = this;
-      Ember.run.later((function () {
-        _this.set("request", false);
-      }), 4000);
-    },
-    verify: function(started_app)
-    {
-      if (started_app)
-      {
-        var con = window.confirm("There is a deployed application currently running on this lambda-instance.\nIf you want to stop it press ok and then press again the STOP button.");
-        if (con)
-        {
-          this.set("verify", false);
-          this.set("confirm", true);
-        }
-        else{
-          this.set("verify", true);
-        }
-      }
-      else {
-        this.set("confirm", false);
-        var _this = this;
-        Ember.run.later((function () {
-          _this.set("request", false);
-        }), 4000);
-      }
-    },
-  },
 });
