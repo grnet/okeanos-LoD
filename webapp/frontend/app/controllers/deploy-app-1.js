ef7720fb3237a172d0d4ed244eb47c59feb0aa95
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-11-05 01:40:20 +0200

    Transition to lambda instance/application, after deploying an application

--- webapp/frontend/app/controllers/deploy-app-1.js
+++ webapp/frontend/app/controllers/deploy-app-1.js
@@ -3,31 +3,13 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   actions: {
     deploy(application_id, instance_id) {
-      var instances;
-      var _this = this;
-      Ember.run.later((function () {
-        this.store.unloadAll('lambda-instance');
-        this.modelFor('lambda-app').application.reload();
-        let ids = this.store.peekAll('lambda-instance').getEach('id');
-        instances = _this.store.filter('lambda-instance', {},
-          function (li) {
-            if (li.get('status_code') !== 0) {
-              return false;
-            }
-            let id = li.get('id');
-            for (var i = 0; i < ids.length; i++) {
-              if (id === ids[i]) {
-                return false;
-              }
-            }
-            return true;
-          });
-      }), 1000);
-
-      _this.set("request", true);
-      Ember.run.later((function () {
-        _this.set("request", false);
-      }), 2500);
+      if (!this.get("failure")) {
+        var _this = this;
+        Ember.run.later((function () {
+          _this.set("request", false);
+          _this.transitionToRoute('lambda-instance', instance_id);
+        }), 3000);
+      }
     }
   }
 });
--- webapp/frontend/app/controllers/deploy-app-2.js
+++ webapp/frontend/app/controllers/deploy-app-2.js
@@ -3,31 +3,13 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   actions: {
     deploy(application_id) {
-      var applications;
-      var _this = this;
-      Ember.run.later((function () {
-        this.store.unloadAll('lambda-app');
-        this.modelFor('lambda-instance').application.reload();
-        let ids = this.store.peekAll('lambda-app').getEach('id');
-        applications = _this.store.filter('lambda-app', {},
-          function (application) {
-            if (application.get('status_code') !== 0) {
-              return false;
-            }
-            let id = application.get('id');
-            for (var i = 0; i < ids.length; i++) {
-              if (id === ids[i]) {
-                return false;
-              }
-            }
-            return true;
-          });
-      }), 1000);
-
-      _this.set("request", true);
-      Ember.run.later((function () {
-        _this.set("request", false);
-      }), 2500);
+      if (!this.get("failure")) {
+        var _this = this;
+        Ember.run.later((function () {
+          _this.set("request", false);
+          _this.transitionToRoute('lambda-app', application_id);
+        }), 3000);
+      }
     }
   }
 });
