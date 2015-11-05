e03edcd3e9bae371167562ee3283e79b3bcc967d
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-11-06 01:22:24 +0200

    Additional info for lambda app details

--- webapp/frontend/app/models/lambda-app.js
+++ webapp/frontend/app/models/lambda-app.js
@@ -9,8 +9,10 @@ var LambdaApp = DS.Model.extend({
   status_message: DS.attr(),
   status_code: DS.attr('number'),
   status_detail: DS.attr(),
+  status_failure_message: DS.attr(),
   lambda_instances: DS.hasMany('lambda-instance'),
-  started: DS.attr('boolean')
+  started: DS.attr('boolean'),
+  execution_environment_name: DS.attr()
 });
 
 export default LambdaApp;
