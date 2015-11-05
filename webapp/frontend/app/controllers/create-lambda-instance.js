2285b4c66dd96574554258f17283529976004ef1
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2015-11-05 12:06:37 +0200

    Added alert message in create lambda instance web page in case the user has no quotas.

--- webapp/frontend/app/controllers/create-lambda-instance.js
+++ webapp/frontend/app/controllers/create-lambda-instance.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+  enoughQuotas: false,
   actions: {
     saveLambdaInstance: function(newLambdaInstance){
       var self = this;
