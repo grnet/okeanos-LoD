c59071738e08bd0d397ced61cd27bea4f6b1497f
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2015-12-18 14:27:34 +0200

    Display different error message, if file already exists

--- webapp/frontend/app/controllers/lambda-apps/upload.js
+++ webapp/frontend/app/controllers/lambda-apps/upload.js
@@ -93,15 +93,21 @@ var UploadController = Ember.Controller.extend({
           });
         },
         statusCode: {
-          400: function() {
-            progress.className = "progress-bar progress-bar-danger";
-            progress.innerHTML =  'Failure. Your request to upload the application has failed.Try another application';
-            progress_text.innerHTML =  '';
+          400: function (response) {
+            progress.clasName = "progress-bar progress-bar-danger";
+            var error_detail = response.responseJSON.errors[0].detail;
+            if (error_detail === 'The specified file name already exists.') {
+              progress.innerHTML = error_detail + ' Try another application.';
+            }
+            else {
+              progress.innerHTML = 'Failure. Your request to upload the application has failed. Try another application.';
+            }
+            progress_text.innerHTML = '';
           }
         },
         error: function() {
           progress.className = "progress-bar progress-bar-danger";
-          progress.innerHTML =  'Your request to application the file has been rejected.Please try again later.';
+          progress.innerHTML =  'Your request to application the file has been rejected. Please try again later.';
           progress_text.innerHTML =  '';
         }
       });
