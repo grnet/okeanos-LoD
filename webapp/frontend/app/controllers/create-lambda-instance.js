06c289562583f8ac111a94f085c073bb56069400
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2016-03-21 17:35:52 +0200

    Refactored create lambda instance form and the way drop down lists are filled with data

--- webapp/frontend/app/controllers/create-lambda-instance.js
+++ webapp/frontend/app/controllers/create-lambda-instance.js
@@ -1,19 +1,115 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+  selectedProject: null,
+  selectedNumberOfSlaves: null,
+  selectedMasterNodeCPUs: null,
+  selectedSlaveNodeCPUs: null,
+  selectedMasterNodeRAM: null,
+  selectedSlaveNodeRAM: null,
+  selectedMasterNodeDisk: null,
+  selectedSlaveNodeDisk: null,
+
+  masterNodeCPUValues: [],
+  slaveNodeCPUValues: [],
+  masterNodeRAMValues: [],
+  slaveNodeRAMValues: [],
+  masterNodeDiskValues: [],
+  slaveNodeDiskValues: [],
+
+  minQuotasPerVM: {'cpus': 2, 'ram': 2048, 'disk': 10},
+  minQuotasPerProject: {
+    'vms': 2,
+    'cpus': 4,
+    'ram': {"bytes": 4294967296, "megaBytes": 4096},
+    'disk': {"bytes": 21474836480, "gigaBytes": 20},
+    'floatingIPs': 1,
+    'privateNetworks': 1
+  },
+
   enoughQuotas: false,
+
   actions: {
     saveLambdaInstance: function(newLambdaInstance){
-      var self = this;
-       newLambdaInstance.save().then(function(){
-        self.transitionToRoute('lambda-instance', newLambdaInstance.get('id')).catch(function() {
-          self.transitionToRoute('lambda-instances.index').then(function(newRoute) {
-            newRoute.controller.set('message', 'Your lambda instance creation will begin shortly.');
-            newRoute.controller.set('request', true);
-            newRoute.controller.send('start_stop');
+      newLambdaInstance.set('instanceName', this.get('instanceName'));
+      newLambdaInstance.set('masterName', this.get('masterName'));
+
+      newLambdaInstance.set('slaves', this.$("input[name='slaves']")[0].value);
+
+      newLambdaInstance.set('projectName', this.$("select[name='okeanos_project']")[0].value);
+      newLambdaInstance.set('VCPUsMaster', this.$("select[name='vcpus_master']")[0].value);
+      newLambdaInstance.set('VCPUsSlave', this.$("select[name='vcpus_slave']")[0].value);
+      newLambdaInstance.set('RamMaster', this.$("select[name='ram_master']")[0].value);
+      newLambdaInstance.set('RamSlave', this.$("select[name='ram_slave']")[0].value);
+      newLambdaInstance.set('DiskMaster', this.$("select[name='disk_master']")[0].value);
+      newLambdaInstance.set('DiskSlave', this.$("select[name='disk_slave']")[0].value);
+
+      var requestedPublicKeys = [];
+      var options = this.$("select[name='public_key_name']")[0].options;
+      for(var i = 0;i < options.length;i++){
+        if(options[i].selected){
+          requestedPublicKeys.push(options[i].value);
+        }
+      }
+      newLambdaInstance.set('publicKeyName', requestedPublicKeys);
+
+      var kafkaOutputTopics, kafkaInputTopics;
+
+      var kafkaInputTopicsString = this.get('kafkaInputTopics');
+      if (kafkaInputTopicsString) {
+        kafkaInputTopics = kafkaInputTopicsString.replace(/\s+/g, '').split(',');
+      }
+
+      var kafkaOutputTopicsString = this.get('kafkaOutputTopics');
+      if (kafkaOutputTopicsString) {
+        kafkaOutputTopics = kafkaOutputTopicsString.replace(/\s+/g, '').split(',');
+      }
+
+      var duplicateTopic = false;
+      if (kafkaInputTopics && kafkaOutputTopics) {
+        kafkaInputTopics.forEach(function (inputTopic) {
+          if (kafkaOutputTopics.indexOf(inputTopic) !== -1) {
+            duplicateTopic = true;
+          }
+        });
+      }
+
+      if (duplicateTopic) {
+        this.set('duplicate_message', 'Apache Kafka input and output topics must be different!');
+        this.set('duplicate', true);
+        document.getElementById('inputTopics').focus();
+      }
+      else {
+        newLambdaInstance.set('kafkaInputTopics', kafkaInputTopics);
+        newLambdaInstance.set('kafkaOutputTopics', kafkaOutputTopics);
+
+        var self = this;
+        newLambdaInstance.save().then(function(){
+          self.transitionToRoute('lambda-instance', newLambdaInstance.get('id')).catch(function() {
+            self.transitionToRoute('lambda-instances.index').then(function(newRoute) {
+              newRoute.controller.set('message', 'Your lambda instance creation will begin shortly.');
+              newRoute.controller.set('request', true);
+              newRoute.controller.send('start_stop');
+            });
           });
         });
-      });
+      }
+    },
+
+    close_alert: function()
+    {
+      this.set('duplicate', false);
+    },
+
+    selectFromDropDownList: function(variable, event){
+      var value = event.target.value;
+      this.set(variable, value);
+
+      this.calculateDropDownListValues();
     }
+  },
+
+  calculateDropDownListValues: function(){
+
   }
 });
--- webapp/frontend/app/routes/create-lambda-instance.js
+++ webapp/frontend/app/routes/create-lambda-instance.js
@@ -15,21 +15,44 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     // has enough quotas to create a Lambda Instance, set enoughQuotas to true.
     // Delete every project that doesn't have the quotas to create the smallest Lambda
     // Instance.
+    var controller = this.controllerFor('create-lambda-instance');
+    var minQuotasPerProject = controller.get('minQuotasPerProject');
     for (var i = 0;i < model.userOkeanosProjects.get('length');i++){
-    	if (model.userOkeanosProjects.objectAt(i).get('vm') >= 2 &&
-    	    model.userOkeanosProjects.objectAt(i).get('cpu') >= 4 &&
-    	    model.userOkeanosProjects.objectAt(i).get('ram') >= 4294967296 &&
-    	    model.userOkeanosProjects.objectAt(i).get('disk') >= 21474836480 &&
-    	    model.userOkeanosProjects.objectAt(i).get('floating_ip') >= 1 &&
-    	    model.userOkeanosProjects.objectAt(i).get('private_network') >= 1) {
+    	if (model.userOkeanosProjects.objectAt(i).get('vm') >= minQuotasPerProject['vms'] &&
+    	    model.userOkeanosProjects.objectAt(i).get('cpu') >= minQuotasPerProject['cpus'] &&
+    	    model.userOkeanosProjects.objectAt(i).get('ram') >= minQuotasPerProject['ram']['bytes'] &&
+    	    model.userOkeanosProjects.objectAt(i).get('disk') >= minQuotasPerProject['disk']['bytes'] &&
+    	    model.userOkeanosProjects.objectAt(i).get('floating_ip') >= minQuotasPerProject['floatingIPs'] &&
+    	    model.userOkeanosProjects.objectAt(i).get('private_network') >= minQuotasPerProject['privateNetworks']) {
 
-        if(!this.controllerFor('create-lambda-instance').get('enoughQuotas')){
-    		  this.controllerFor('create-lambda-instance').set('enoughQuotas', true);
+        if(!controller.get('enoughQuotas')){
+    		  controller.set('enoughQuotas', true);
         }
     	}
       else{
         model.userOkeanosProjects.objectAt(i).deleteRecord();
       }
+    }
+
+    // If at least one project has enough quotas, set the default values for the drop down lists.
+    if(model.userOkeanosProjects.get('length') > 0){
+      controller.set('selectedProject', model.userOkeanosProjects.objectAt(0).get('name'));
+      controller.set('selectedNumberOfSlaves', minQuotasPerProject['vms'] - 1);
+
+      var minQuotasPerVM = controller.get('minQuotasPerVM');
+      controller.set('selectedMasterNodeCPUs', minQuotasPerVM['cpus']);
+      controller.set('selectedSlaveNodeCPUs', minQuotasPerVM['cpus']);
+      controller.set('selectedMasterNodeRAM', minQuotasPerVM['ram']);
+      controller.set('selectedSlaveNodeRAM', minQuotasPerVM['ram']);
+      controller.set('selectedMasterNodeDisk', minQuotasPerVM['disk']);
+      controller.set('selectedSlaveNodeDisk', minQuotasPerVM['disk']);
+
+      controller.set('masterNodeCPUValues', model.VMParameterValues.objectAt(0).get('vcpus'));
+      controller.set('slaveNodeCPUValues', model.VMParameterValues.objectAt(0).get('vcpus'));
+      controller.set('masterNodeRAMValues', model.VMParameterValues.objectAt(0).get('ram'));
+      controller.set('slaveNodeRAMValues', model.VMParameterValues.objectAt(0).get('ram'));
+      controller.set('masterNodeDiskValues', model.VMParameterValues.objectAt(0).get('disk'));
+      controller.set('slaveNodeDiskValues', model.VMParameterValues.objectAt(0).get('disk'));
     }
   }
 });
