e1b75cb298a03df71fe8b94bf8223184fb29592a
Georgios Ouzounis <gouzouni@lab.grid.auth.gr>
2016-04-29 11:52:07 +0300

    Dynamically configure max upload file size in ansible

--- webapp/frontend/render_ember_environment.py
+++ webapp/frontend/render_ember_environment.py
@@ -1,12 +1,18 @@
 from jinja2 import Environment, FileSystemLoader
 import socket
 import os
+import yaml
 
 if __name__ == '__main__':
     env = Environment(loader=FileSystemLoader('../ansible/roles/ember/templates'))
     template = env.get_template('environment.js.j2')
     ansible_hostname = socket.gethostname()
-    settings = template.render(ansible_hostname=ansible_hostname)
+
+    variables_file = open("../ansible/roles/service-vm/vars/main.yml")
+    max_upload_file_size = yaml.load_all(variables_file).next()['max_upload_file_size']
+
+    settings = template.render(ansible_hostname=ansible_hostname,
+                               max_upload_file_size=max_upload_file_size)
     if not os.path.exists('config'):
         os.mkdir('config')
     with open('config/environment.js', 'w') as f:
